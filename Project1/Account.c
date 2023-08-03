763096cffbee2639f6c375c7b88b9ca4d1bf5ae5
zeeebadshah <mbadshah4394@conestogac.on.ca>
2023-08-03 10:51:11 -0400

    deleteAccount funcntion created

--- Project1/Account.c
+++ Project1/Account.c
@@ -37,3 +37,41 @@ void addNewAccount() {
 
     printf("\nNew account added successfully!\n");
 }
+void deleteAccount() {
+    int accountNumber;
+    int accountIndex = -1;
+    printf("\nEnter account number to delete: ");
+    if (scanf("%d", &accountNumber) != 1) {
+        printf("Error: Invalid input");
+        return;
+    }
+
+    // Find the index of the account to be deleted
+    for (int i = 0; i < numAccounts; i++) {
+        if (accounts[i].accountNumber == accountNumber) {
+            accountIndex = i;
+            break;
+        }
+    }
+
+    // Delete the account from the array
+    if (accountIndex == -1) {
+        printf("\nAccount not found!\n");
+    }
+    else {
+        for (int i = accountIndex; i < numAccounts - 1; i++) {
+            accounts[i] = accounts[i + 1];
+        }
+        numAccounts--;
+        struct BankAccount* tmpPtr;
+        tmpPtr = (struct BankAccount*)realloc(accounts, numAccounts * sizeof(struct BankAccount));
+        if (tmpPtr == NULL) {
+            printf("Error: Memory allocation failed");
+            // handle the error appropriately (e.g., exit the program)
+        }
+        else {
+            accounts = tmpPtr;
+        }
+        printf("\nAccount deleted successfully!\n");
+    }
+}
