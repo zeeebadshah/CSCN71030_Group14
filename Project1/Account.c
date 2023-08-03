d2bb1e90a362ffc5d024a86fb335b80c1c364387
zeeebadshah <mbadshah4394@conestogac.on.ca>
2023-08-03 10:51:48 -0400

    updateAccount function created

--- Project1/Account.c
+++ Project1/Account.c
@@ -75,3 +75,86 @@ void deleteAccount() {
         printf("\nAccount deleted successfully!\n");
     }
 }
+void updateAccount() {
+    int accountNumber;
+    int accountIndex = -1;
+    printf("\nEnter account number to update: ");
+    if (scanf("%d", &accountNumber) != 1) {
+        printf("\nInvalid Input\n");
+        return;
+    }
+
+    // Find the index of the account to be updated
+    for (int i = 0; i < numAccounts; i++) {
+        if (accounts[i].accountNumber == accountNumber) {
+            accountIndex = i;
+            break;
+        }
+    }
+
+    // Update the account details
+    if (accountIndex == -1) {
+        printf("\nAccount not found!\n");
+    }
+    else {
+        printf("\n1. Update account holder name\n");
+        printf("2. Update account balance\n");
+        printf("3. Deposit money\n");
+        printf("4. Withdraw money\n");
+        printf("\nEnter your choice: ");
+        int choice;
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid Choice\n");
+            return;
+        }
+
+        switch (choice) {
+        case 1:
+            printf("\nEnter new account holder name: ");
+            if (scanf("%s", accounts[accountIndex].accountHolderName) != 1) {
+                printf("Invalid Input\n");
+                return;
+            }
+            printf("\nAccount holder name updated successfully!\n");
+            break;
+
+        case 2:
+            printf("Enter new account balance: ");
+            if (scanf("%f", &accounts[accountIndex].accountBalance) != 1) {
+                printf("Invalid Input\n");
+                return;
+            }
+            printf("\nAccount balance updated successfully!\n");
+            break;
+
+        case 3:
+            printf("Enter the amount to be deposited: ");
+            float depositAmount;
+            if (scanf("%f", &depositAmount) != 1) {
+                printf("Invalid Input\n");
+            }
+            accounts[accountIndex].accountBalance += depositAmount;
+            printf("\nAmount deposited successfully!\n");
+            break;
+
+        case 4:
+            printf("Enter the amount to be withdrawn: ");
+            float withdrawAmount;
+            if (scanf("%f", &withdrawAmount) != 1) {
+                printf("Invalid Input\n");
+                return;
+            }
+            if (withdrawAmount > accounts[accountIndex].accountBalance) {
+                printf("\nInsufficient balance!\n");
+            }
+            else {
+                accounts[accountIndex].accountBalance -= withdrawAmount;
+                printf("\nAmount withdrawn successfully!\n");
+            }
+            break;
+
+        default:
+            printf("\nInvalid choice!\n");
+        }
+    }
+}
